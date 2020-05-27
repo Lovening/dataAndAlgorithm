8a37ef2a942ae3d4d89baab258385f467639cfaf
Lovening <wuxiaoning523@163.com>
2020-05-28 00:16:54 +0800

    插入排序

---
+++ insertion/cmake-build-debug/CMakeFiles/insertion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/insertion.dir/main.cpp.obj"
+  "insertion.exe"
+  "insertion.exe.manifest"
+  "insertion.pdb"
+  "libinsertion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/insertion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ insertion/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using  namespace std;
+//void insertionSort(int *a,int len)
+//{
+//    int in,out,temp;
+//    for(out = 1;out< len ;++out){
+//        in = out;
+//        temp = a[out];
+//        while (in > 0 && a[in-1] >= temp){
+//                a[in] = a[in-1];
+//                -- in;
+//        }
+//        a[in] = temp;
+//    }
+//
+//}
+
+template <typename T> void insertionSort( T* a,int len)
+{
+    int in,out,temp;
+    for(out = 1;out< len ;++out){
+        in = out;
+        temp = a[out];
+        while (in > 0 && a[in-1] >= temp){
+                a[in] = a[in-1];
+                -- in;
+        }
+        a[in] = temp;
+    }
+}
+
+int main() {
+    int a[]={2,8,1,4,5,3,7,10};
+    insertionSort(a,8);
+    for(int i = 0;i<8;++i){
+        cout<<a[i]<<endl;
+    }
+    return 0;
+}
