409986079088f2ba21b27c41353eb59f8be1f4e7
Lovening <wuxiaoning523@163.com>
2020-06-01 23:40:47 +0800

    快速排序

---
+++ quickSort/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+template <typename  T>
+void  quickSort(T* a,int left,int right)
+{
+
+}
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
